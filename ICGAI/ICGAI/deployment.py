9cb36b95a28e254865d6266c8b932db05f13f622
icgmedical-developers <ICGdeveloper.DBA@icg-medical.com>
2024-10-10 13:54:43 +0100

    yml change

--- ICGAI/ICGAI/deployment.py
+++ ICGAI/ICGAI/deployment.py
@@ -24,17 +24,17 @@ MIDDLEWARE = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
-# parameters = {pair.split('=')[0]:pair.split('=')[1] for pair in connection_string.split(' ')}
+connection_string = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
+parameters = {pair.split('=')[0]:pair.split('=')[1] for pair in connection_string.split(' ')}
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': parameters['dbname'],
-#         'HOST': parameters['host'],
-#         'USER': parameters['user'],
-#         'PASSWORD': parameters['password'],
-#         'PORT': parameters['port']
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': parameters['dbname'],
+        'HOST': parameters['host'],
+        'USER': parameters['user'],
+        'PASSWORD': parameters['password'],
+        'PORT': parameters['port']
+    }
+}
